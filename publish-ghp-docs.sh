db0b351e364a8feb69df11499bda208881c994ff
Trilokesh Rangam <rangam.lokesh@gmail.com>
2023-01-02 23:10:40 +0530

    lfs tracking enabled
    Signed-off-by: Trilokesh Rangam <rangam.lokesh@gmail.com>

--- publish-ghp-docs.sh
+++ publish-ghp-docs.sh
@@ -1,14 +1,3 @@
-#!/bin/bash
-
-git describe --exact-match --tags $(git log -n1 --pretty='%h') >/dev/null 2>&1
-if [[ $? != 0 ]]; then
-    echo "Should not publish tags from an untagged commit!"
-    exit 1
-fi
-
-cd $(git rev-parse --show-toplevel)
-rm -rf target/doc/
-rustup run nightly cargo doc --no-deps --features "backup blob chrono functions limits load_extension serde_json trace"
-echo '<meta http-equiv=refresh content=0;url=rusqlite/index.html>' > target/doc/index.html
-ghp-import target/doc
-git push origin gh-pages:gh-pages
+version https://git-lfs.github.com/spec/v1
+oid sha256:3e2c59e09ce377c46a62511aa17389e6bf824c17d2fc5a818409ede40ba64c23
+size 512
